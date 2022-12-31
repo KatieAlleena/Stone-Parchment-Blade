7743b3bf8da03c39365531455de4005ca78b6de6
Katie Morton <kmalleena@gmail.com>
2022-12-31 02:48:06 -0500

    copied over project & updated readme & design. Pending more design changes.

--- README.md
+++ README.md
@@ -1,6 +1,79 @@
+<pre>
 # Stone-Parchment-Blade
 Final Project for CanCode's Javascript Fundamentals Class. It's a Legend of Zelda themed Rock, Paper, Scissors game done in JavaScript, HTML, and CSS!
 
 
-Change Log: 
-12/30/2022 -- Initial Commit
+Change Log:
+12/13/2022 -- Research phase and first attempts
+12/20/2022 -- Decided to change to Legend of Zelda Theme
+12/30/2022 -- Initial Commit / creation of github repo
+12/31/2022 -- Upload of project, using temp images while awaiting art assets to be completed.
+
+Live Version: https://katiealleena.github.io/Stone-Parchment-Blade/
+
+Instructions: 
+
+Play Stone, Parchment, Blade against the CPU and somehow, save Hyrule. (: 
+
+Stone crushes Blade
+Parchment covers Stone
+Blade slices Paper
+
+On launch, click your choice! The computer will pick something random and you'll be informed if you've won, lost or tied that round. 
+
+On a win, you get 1 point.
+On a lose, the CPU gets 1 point.
+On a draw, neither of you get a point.
+
+The game ends when the player or the CPU gets 5 points.
+
+Regardless of outcome, simply click your next choice to reset the scores and play another round.
+
+
+
+***
+
+Difficulties: 
+Had to research how to make the computer make choices by itself. We used math.floor in hangman so it was kinda familiar a little bit. 
+
+Forgot a few CSS tricks. Had to spend a chunk of time researching! 
+
+Changed theme from boring old rock paper scissors to Zelda Theme. Added more work for myself in having to create art.
+
+I don't know where to begin to show a pop up victory or loss screen... I kinda wanted to draw more cute fanart? I may skip this. 
+
+
+Resources & References: 
+1. https://www.w3schools.com/tags/att_script_defer.asp
+(This allows everything to load in at the same time. Needed so things were immediately clickable when the page was ready.)
+
+2. https://stackoverflow.com/questions/8715860/what-does-mean-in-css
+3. https://developer.mozilla.org/en-US/docs/Web/CSS/::before
+4. https://developer.mozilla.org/en-US/docs/Web/CSS/::after
+(These fixed a problem with things looking off in other browsers -- it resets everything, more or less.)
+
+5. https://www.w3.org/Style/Examples/007/fonts.en.html
+6. https://fonts.google.com/specimen/Macondo/tester?category=Display
+7. https://www.w3schools.com/css/css_font_google.asp
+8. https://www.w3schools.com/css/css_border.asp
+9. https://www.w3schools.com/cssref/pr_font_weight.php
+10. https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Values_and_units
+(Some CSS resources and reminders)
+
+11. https://www.geeksforgeeks.org/rock-paper-and-scissor-game-using-javascript/
+12. https://www.youtube.com/watch?v=n1_vHArDBRA
+13. https://eloquentjavascript.net/15_event.html
+14. https://www.youtube.com/watch?v=RwFeg0cEZvQ
+15. https://stackoverflow.com/questions/17976883/rock-paper-scissors-in-javascript
+16. https://betterprogramming.pub/7-ways-to-code-rock-paper-scissors-in-javascript-4189a5e7e535
+17. https://codereview.stackexchange.com/questions/276508/rock-paper-and-scissors-game
+
+(Some resources for understanding the game itself in Javascript.)
+
+18. https://icons8.com/icon/17465/triforce
+19. http://botw-recipes.com/
+20. https://www.instagram.com/artofkatiealleena/
+(Image assets used from other sources)
+
+
+</pre>
---
+++ script.js
@@ -0,0 +1,147 @@
+(function () { 
+
+/* I am choosing to keep using Rock, Paper, Scissors as variables, as I decided on the Zelda theme during the design phase.  */
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const choices = [rock, paper, scissors];
+
+const resolution = document.querySelector(".resolution");
+const playerPickDisplay = document.querySelector(".player-pick");
+const playerScoreDisplay = document.querySelector(".player-score");
+const cpuPickDisplay = document.querySelector(".cpu-pick");
+const cpuScoreDisplay = document.querySelector(".cpu-score");
+const roundNumberDisplay = document.querySelector(".round-number");
+
+let playerScoreNumber = 0;
+let cpuScoreNumber = 0;
+let roundNumber = 0;
+
+// this is how the player chooses! 
+
+rock.addEventListener("click", () => {
+   return compareSelection(rock);
+});
+paper.addEventListener("click", () => {
+   return compareSelection(paper);
+});
+scissors.addEventListener("click", () => {
+   return compareSelection(scissors);
+});
+
+// this is how CPU will choose!
+const getComputerChoice = () => {
+   return choices[Math.floor(Math.random() * 3)];
+};
+
+
+const updateDisplay = (playerChoice, cpu) => {
+   choices.forEach((event) => {
+      if (event.classList) {
+         event.classList.remove("player-active", "cpu-active");
+      }
+   });
+
+   // This is the game itself, and how our choices affect a win or loss condition compared to the CPU.
+
+   if (playerChoice === rock) {
+      playerPickDisplay.textContent = "Stone";
+      rock.classList.add("player-active");
+   } else if (playerChoice === paper) {
+      playerPickDisplay.textContent = "Parchment";
+      paper.classList.add("player-active");
+   } else {
+      playerPickDisplay.textContent = "Blade";
+      scissors.classList.add("player-active");
+   }
+
+   if (cpu === rock) {
+      cpuPickDisplay.textContent = "Stone";
+      rock.classList.add("cpu-active");
+   } else if (cpu === paper) {
+      cpuPickDisplay.textContent = "Parchment";
+      paper.classList.add("cpu-active");
+   } else {
+      cpuPickDisplay.textContent = "Blade";
+      scissors.classList.add("cpu-active");
+   }
+};
+
+// This changes the text depending on choice results.
+const compareSelection = (playerChoice) => {
+   const cpu = getComputerChoice();
+   updateDisplay(playerChoice, cpu);
+
+   if (playerChoice === cpu) {
+      resolution.textContent = "It's a draw!";
+      resolution.style.color = "turquoise";
+      return tieRound();
+   } else if (
+      (playerChoice === rock && cpu === paper) ||
+      (playerChoice === paper && cpu === scissors) ||
+      (playerChoice === scissors && cpu === rock)
+   ) {
+      resolution.textContent = "You've lost!";
+      resolution.style.color = "palevioletred";
+      return loseRound();
+   } else {
+      resolution.textContent = "You've won!";
+      resolution.style.color = "lawngreen";
+      return winRound();
+   }
+};
+// this keeps track of how many rounds are won or lost and keeps the game going until the win condition of 5 is met. 
+
+const tieRound = () => {
+   roundNumberDisplay.textContent = ++roundNumber;
+};
+
+const winRound = () => {
+   playerScoreDisplay.textContent = ++playerScoreNumber;
+
+   roundNumberDisplay.textContent = ++roundNumber;
+
+   if (playerScoreNumber === 5) {
+      return resetGame();
+   }
+};
+
+const loseRound = () => {
+   cpuScoreDisplay.textContent = ++cpuScoreNumber;
+
+   roundNumberDisplay.textContent = ++roundNumber;
+
+   if (cpuScoreNumber === 5) {
+      return resetGame();
+   }
+};
+
+// resets the game with either result
+
+const resetGame = () => {
+   if (playerScoreNumber === 5) {
+      resolution.textContent = "The Hero prevails! Click to play again.";
+      resolution.style.color = "lawngreen";
+   } else {
+      resolution.textContent = "This is the downfall timeline, it seems. Click to play again!";
+      resolution.style.color = "palevioletred";
+   }
+
+   // This updates the counters and round numbers!
+
+   playerScoreNumber = 0;
+   cpuScoreNumber = 0;
+   roundNumber = 0;
+
+   playerScoreDisplay.textContent = playerScoreNumber;
+   cpuScoreDisplay.textContent = cpuScoreNumber;
+   roundNumberDisplay.textContent = roundNumber;
+   choices.forEach((event) => {
+      if (event.classList) {
+         event.classList.remove("player-active", "cpu-active");
+      }
+   });
+};
+
+})();
